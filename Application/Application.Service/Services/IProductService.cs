b0d1abe79f9caf2d6271df941b3a7d96de6ba03d
sumanmis <suman.mishra@epsilon.com>
2018-04-05 22:09:38 +0530

    added

---
+++ Application/Application.Service/Services/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Service.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+        Product GetProduct(int id);
+    }
+}
---
+++ Application/Application.Service/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Application.Data;
+
+
+namespace Application.Service.Services
+{
+    public class ProductService : IProductService
+    {
+        private ProductsEntities db = new ProductsEntities();
+        public Product GetProduct(int id)
+        {
+            return db.Products.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<Product> GetProducts()
+        {
+            return db.Products.ToList();
+        }
+    }
+}
